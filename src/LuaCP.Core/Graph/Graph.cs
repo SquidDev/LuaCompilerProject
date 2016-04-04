d2b8104c23e2419233b04ff2089d70c9bf80b64f
SquidDev <bonzoweb@hotmail.co.uk>
2016-04-04 16:23:59 +0100

    Add primitive graph colourer

---
+++ src/LuaCP.Core/Graph/Graph.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LuaCP.Graph
+{
+	/// <summary>
+	/// A basic undirected graph
+	/// </summary>
+	public sealed class Graph
+	{
+		private readonly bool[,] neighbours;
+		private readonly int nodes;
+
+		public Graph(int nodes)
+		{
+			this.nodes = nodes;
+			this.neighbours = new bool[nodes, nodes];
+		}
+
+		public void AddEdge(int left, int right)
+		{
+			neighbours[left, right] = true;
+		}
+
+		public void AddEdges(IEnumerable<Tuple<int, int>> edges)
+		{
+			foreach (var edge in edges)
+			{
+				AddEdge(edge.Item1, edge.Item2);
+			}
+		}
+
+		public IEnumerable<int> Neighbours(int node)
+		{
+			for (int i = 0; i < Size; i++)
+			{
+				if (neighbours[node, i]) yield return i;
+			}
+		}
+
+		public int Size { get { return nodes; } }
+	}
+}
+
